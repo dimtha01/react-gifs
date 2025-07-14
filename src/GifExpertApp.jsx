69ea6f1e6c86af1001749b72aa60f82dff215420
dimtha01 <richarsonmartinez400@gmail.com>
2025-07-14 03:18:38 -0400

    feat: Agregar estado y listado de categorias
    * Implementar gestión de categorías con useState
    * Crear lista ordenada de categorías en UI
    * Agregar registro de categorías en consola para depuración
    * Definir estado inicial con valores predeterminados
    * Asignar key único a elementos de lista mediante índice

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
 
 export const GifExpertApp = () => {
+  
+  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+
+  console.log(categories);
+
   return (
     <>
       <h1>GifEsperApp</h1>
+
       {/* Input */}
 
       {/* Listados de gif */}
+      <ol>
+        {categories.map((categorie, index) => {
+          return <li key={index}>{categorie}</li>
+        })}
+      </ol>
       {/* git Item */}
     </>
   )
